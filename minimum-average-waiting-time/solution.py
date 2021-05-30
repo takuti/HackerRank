732f0a0c400bab5f20cdd232fd51fe98e1020ca4
Takuya Kitazawa <k.takuti@gmail.com>
2021-05-29 20:36:59 -0700

    Implement PriorityQueue myself

--- minimum-average-waiting-time/solution.py
+++ minimum-average-waiting-time/solution.py
@@ -5,7 +5,57 @@ import os
 import random
 import re
 import sys
-from heapq import heappush, heappop
+
+
+class PriorityQueue(object):
+
+    def __init__(self):
+        self.q = []
+
+    def parent(self, i):
+        return (i - 1) // 2
+
+    def left(self, i):
+        return i * 2 + 1
+
+    def right(self, i):
+        return i * 2 + 2
+
+    def up(self, i):
+        j = self.parent(i)
+        while (i > 0 and self.q[j] > self.q[i]):
+            self.q[i], self.q[j] = self.q[j], self.q[i]
+            i, j = j, self.parent(j)
+
+    def down(self, i):
+        k = i
+
+        l = self.left(i)
+        if l < len(self.q) and self.q[l] < self.q[k]:
+            k = l
+
+        r = self.right(i)
+        if r < len(self.q) and self.q[r] < self.q[k]:
+            k = r
+
+        if k != i:
+            self.q[i], self.q[k] = self.q[k], self.q[i]
+            self.down(k)
+
+    def push(self, e):
+        self.q.append(e)
+        self.up(len(self.q) - 1)
+
+    def pop(self):
+        e = self.q[0]
+        self.q[0] = self.q[-1]
+        del self.q[-1]
+        self.down(0)
+        return e
+
+    def __len__(self):
+        return len(self.q)
+
 
 #
 # Complete the 'minimumAverage' function below.
@@ -18,16 +68,16 @@ def minimumAverage(customers):
     customers = sorted(customers)
 
     t = customers[0][0]  # first customer's arrival time
-    queue = []
+    queue = PriorityQueue()
     for c in customers:
         if c[0] == t:
-            heappush(queue, c[::-1])
+            queue.push(c[::-1])
     cnt = len(queue)
 
     accum_waiting = 0
 
     while len(queue) != 0:
-        c = heappop(queue)[::-1]
+        c = queue.pop()[::-1]
 
         t += c[1]  # add time to cook
         accum_waiting += t - c[0]
@@ -38,7 +88,7 @@ def minimumAverage(customers):
             if t >= customers[cnt][0]:
                 for c in customers[cnt:]:
                     if c[0] <= t:
-                        heappush(queue, c[::-1])
+                        queue.push(c[::-1])
                         cnt += 1
 
     return accum_waiting // cnt
