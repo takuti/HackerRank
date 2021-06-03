652da40fc7b8b13fe3d80fd9be6f2633b10b6722
Takuya Kitazawa <k.takuti@gmail.com>
2021-06-02 22:13:02 -0700

    Solve: tree-preorder-traversal

---
+++ trees/tree-preorder-traversal/solution.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    res = []
+    stack = [root]
+    while len(stack) > 0:
+        node = stack.pop(-1)
+        res.append(node.info)
+
+        if node.left is None and node.right is None:
+            continue
+
+        if node.right is not None:
+            stack.append(node.right)
+        if node.left is not None:
+            stack.append(node.left)
+
+    print(' '.join(map(str, res)))
+
+
